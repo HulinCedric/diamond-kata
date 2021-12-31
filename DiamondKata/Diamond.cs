3caf9f8408c410d3a0125ffbd803af2ef280e94e
Cédric Hulin <hulin.cedric@gmail.com>
2021-12-31 15:33:04 +0100

    Character indentation

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -11,18 +11,37 @@ public abstract class Diamond
         if (suppliedLetter == StartingLetter)
             return $"{StartingLetter}";
 
-        var printResult = new StringBuilder($"{StartingLetter}");
-        printResult.Append('\n');
+        var printResult = new StringBuilder();
 
-        for (var nextLetter = GetNextLetter(StartingLetter);
+        for (var nextLetter = StartingLetter;
              nextLetter <= suppliedLetter;
              nextLetter = GetNextLetter(nextLetter))
         {
-            printResult.Append(string.Join(string.Empty, Enumerable.Repeat(nextLetter, 2)));
+            printResult.Append(PrepareLine(suppliedLetter, nextLetter));
             printResult.Append('\n');
         }
 
         return printResult.ToString();
+    }
+
+    private static StringBuilder PrepareLine(char targetLetter, char currentLetter)
+    {
+        var lineResult = new StringBuilder();
+
+        var indentationCount = targetLetter - currentLetter;
+        var indentation = string.Join(string.Empty, Enumerable.Repeat(" ", indentationCount));
+
+        lineResult.Append(indentation);
+
+        var printLetter = $"{currentLetter}";
+        lineResult.Append(printLetter);
+
+        if (currentLetter != StartingLetter)
+            lineResult.Append(printLetter);
+
+        lineResult.Append(indentation);
+
+        return lineResult;
     }
 
     private static char GetNextLetter(char letter)
