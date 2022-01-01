2dbc67d6d2190454d3690db9c8111d69bfd0a5fc
Cédric Hulin <hulin.cedric@gmail.com>
2022-01-01 20:08:14 +0100

    Acceptance test with SpecFlow

---
+++ DiamondKata.Tests/Incremental/ExampleDriven/OutsideIn/Diamond.Specs/Steps/DiamondStepDefinitions.cs
@@ -0,0 +1,27 @@
+﻿using TechTalk.SpecFlow;
+using Xunit;
+
+namespace DiamondKata.Tests.Incremental.ExampleDriven.OutsideIn.Diamond.Specs.Steps;
+
+[Binding]
+public sealed class DiamondStepDefinitions
+{
+    private string actualPrintResult = null!;
+
+    private char suppliedLetter;
+
+    [Given(@"the letter '(.*)'")]
+    public void GivenTheLetter(char letter)
+        => suppliedLetter = letter;
+
+    [When(@"print a diamond")]
+    public void WhenPrintADiamond()
+        => actualPrintResult = DiamondKata.Diamond.Print(suppliedLetter);
+
+    [Then(@"diamond prints")]
+    public void ThenDiamondPrints(string expectedPrintResult)
+    {
+        expectedPrintResult = expectedPrintResult.Replace("\r", "");
+        Assert.Equal(expectedPrintResult, actualPrintResult);
+    }
+}
